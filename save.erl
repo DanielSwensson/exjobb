8b4d0dfdb5b2b81fe115ec8c4aead575965d79c2
Daniel Swensson <danielswensson@hotmail.com>
2014-05-08 15:41:00 +0200

    style changes

--- save.erl
+++ save.erl
@@ -12,20 +12,27 @@ save_to_file(Results,Count,Stddiv,FileName) ->
   write("<!doctype html>\n"),
   write("<html>\n"),
   write("<head>\n"),
+  write("<link href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' rel='stylesheet'>\n"),
+  write("<link href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css' rel='stylesheet'>\n"),
   write("<title>Resultat</title>\n"),
   write("</head>\n"),
   write("<body>\n"),
 
-  write("<h1> Resultat </h1>\n"),
+  write("<div class='container'>\n"),
 
-  write("<b> Antal studenter: " ++ integer_to_list(Count) ++ "</b>\n"),
+  write("<h1 class='page-header'> Resultat "),
+
+  write("<small> Antal studenter: " ++ integer_to_list(Count) ++ "</small></h1>\n"),
 
   write("<h2> Värden </h2>\n"),
-  write("<ul>\n"),
+  % write("<ul>\n"),
   write_stddiv(Stddiv),
-  write("</ul>\n"),
-
+  % write("</ul>\n"),
+  write("<br>\n"),
+  write("<br>\n"),
+  write("<h2> Individuella resultat </h2>\n"),
   write_result_per_student(Results),
+  write("</div>\n"),
   write("</body>\n"),
   write("</html>\n"),
   writer:close(),
@@ -34,24 +41,29 @@ save_to_file(Results,Count,Stddiv,FileName) ->
 write_result_per_student([]) -> ok;
 write_result_per_student([Result | Results]) ->
   {Errors, NrLines,NrComments, Name} = Result,
-  write("<h4> "++ Name ++ "</h4>\n"),
-  write("<b> Lines of code: " ++ integer_to_list(NrLines) ++ "</b><br>\n"),
-  write("<b> Comment lines: " ++ integer_to_list(NrComments) ++ "</b>\n"),
-  write("<ul>\n"),
+   write("<div class='col-md-4'>\n"),
+  write("<h3> "++ Name ++ " "),
+  write("<small> Lines of code: " ++ integer_to_list(NrLines)),
+  write(" Comment lines: " ++ integer_to_list(NrComments) ++ "</small></h3>\n"),
+  
   ResultsList = dict:to_list(Errors),
   write_list(ResultsList),
-  write("</ul>\n"),
+  write("</div>\n"),
   write_result_per_student(Results).
 
 write_list([]) -> ok;
 write_list([{ErrorType, Freq}| T]) ->
-  write("<li>" ++ [ErrorType] ++ " = <b>" ++ integer_to_list(Freq) ++ "</b></li>\n"),
+ 
+  write("<h6>" ++ [ErrorType] ++ ": <small>" ++ integer_to_list(Freq) ++ "</small></h6>\n"),
+  
   write_list(T).
 
 write_stddiv([]) -> ok;
 write_stddiv([{Type, AverageList} | Averages]) ->
   {MedelVarde, Avvikelse} = stddiv:run(AverageList),
-  write("<li><h4>" ++ [Type] ++ "</h4>Avvikelse: " ++ io_lib:format("~.7f",[Avvikelse]) ++ "<br>" ++ " Medelvärde: " ++ io_lib:format("~.7f",[MedelVarde]) ++  "</li>\n"),
+  write("<div class='col-md-4'>\n"),
+  write("<h5>" ++ [Type] ++ "</h5> \n <p>Avvikelse: " ++ io_lib:format("~.7f",[Avvikelse]) ++ "<br>" ++ " Medelvärde: " ++ io_lib:format("~.7f",[MedelVarde]) ++  "</p>\n"),
+  write("</div>\n"),
   write_stddiv(Averages).
 
 
