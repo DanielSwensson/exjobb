d615f37dd4348d19440d5ccaa880226a836329ac
Daniel Swensson <danielswensson@hotmail.com>
2014-05-20 16:18:54 +0200

    adjust script to arrange height of tables per row

--- src/template/script.js
+++ src/template/script.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
     $(".tablesorter").tablesorter();
 
-    boxes = $('.box');
-    maxHeight = Math.max.apply(
-        Math, boxes.map(function() {
-            return $(this).height();
-        }).get());
-    boxes.height(maxHeight);
+    adjustBoxHeight();
+
 });
+
+
+function adjustBoxHeight() {
+    boxes = $('.box');
+
+    var count = 0;
+    var max = 0;
+    var first = 0;
+    var second = 0;
+    boxes.each(function(index) {
+        if (count === 0) {
+            first = $(this);
+            count++;
+        } else if (count === 1) {
+            second = $(this);
+            max = first.height() >= second.height() ? first.height() : second.height();
+            first.height(max);
+            second.height(max);
+            max = 0;
+            count = 0;
+        }
+
+    });
+
+}
